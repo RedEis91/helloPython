e23ab0cc29ed3314327cadad5b08519f9806774a
RedEis91 <imitationkrabs91@gmail.com>
2017-09-08 17:49:55 -0400

    added arithmetic operator examples

--- hellopython.py
+++ hellopython.py
@@ -1,5 +1,12 @@
 import random
 import sys
 import os
+name = "Jared"
+print("Hello " + name)
+print("5 * 2 = ", 5*2)
+print("5 / 2 = ", 5/2)
+print("5 % 2 = ", 5%2)
+print("5 ** 2 = ", 5**2)
+print("5 // 2 = ", 5//2)
 
-print("Hello World")
+
