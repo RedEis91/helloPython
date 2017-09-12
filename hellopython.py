11bb805dccc36047664bfa6130641ee17aed3608
RedEis91 <imitationkrabs91@gmail.com>
2017-09-11 23:53:35 -0400

    added commentary and order of operations examples

--- hellopython.py
+++ hellopython.py
@@ -1,6 +1,12 @@
 import random
 import sys
 import os
+
+# This is my first Python project and my first Python comment! - Jared :D~
+'''
+This code couldn't have been created without the invaluable tutorial from Derek Banas here: https://www.youtube.com/watch?v=N4mEzFDjqtA&t=1937s
+& I'm going to display the powers of Python at work right after this multi-line comment! - Jared :P~
+'''
 name = "Jared"
 print("Hello " + name)
 print("5 * 2 = ", 5*2)
@@ -9,4 +15,6 @@ print("5 % 2 = ", 5%2)
 print("5 ** 2 = ", 5**2)
 print("5 // 2 = ", 5//2)
 
-
+# This code demonstrates order of operations in Python
+print("1 + 2 - 3 * 2 =", 1 + 2 - 3 * 2)
+print("(1 + 2 - 3) * 2 =", (1 + 2 - 3) * 2)
